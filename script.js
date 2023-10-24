97e371b8d21988a0393c0ea35c29828707dae0fe
Rain44556 <bristy44556@gmail.com>
2023-10-24 20:34:11 +0600

    login and signup form are getting ready with js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const wrapper = document.querySelector('.wrapper');
 const logInLink = document.querySelector('.login-link');
 const signUpLink = document.querySelector('.signup-link');
+const btnLogIn = document.querySelector('.btn-login')
 
+
+signUpLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
 
 logInLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
-signUpLink.addEventListener('click', ()=>{
-    wrapper.classList.add('active');
+btnLogIn.addEventListener('click', ()=>{
+    wrapper.classList.add('active-btn');
 });
